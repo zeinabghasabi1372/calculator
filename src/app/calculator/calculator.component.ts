37265081f46015479a5b9260d40be3162baf7dd2
mohsenmesr <mohsenmes@yahoo.com>
2023-11-25 16:45:29 +0330

    started new project

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css'],
+})
+export class CalculatorComponent {}
